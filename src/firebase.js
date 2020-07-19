c64a573d3232718ea9acd273f9b3625c807c2a44
johnny <johnnynguyenwork@gmail.com>
2020-07-19 12:01:07 +1000

    added firebase.js and login/logout

---
+++ src/firebase.js
@@ -0,0 +1,35 @@
+import app from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCJ4lNinmD-aLMlZHSC0mpRAHnWbbEpyuQ",
+    authDomain: "adonis-atlas-app.firebaseapp.com",
+    databaseURL: "https://adonis-atlas-app.firebaseio.com",
+    projectId: "adonis-atlas-app",
+    storageBucket: "adonis-atlas-app.appspot.com",
+    messagingSenderId: "345973023112",
+    appId: "1:345973023112:web:d6f68566f5c20ef8d05a3d"
+};
+
+class Firebase {
+    constructor() {
+        app.initializeApp(firebaseConfig);
+        this.auth = app.auth();
+        this.db = app.firestore();
+    }
+
+    login(email,password){
+        ///REMEMBER: this returns a "promise" 
+        ///-> Like how futures/streams work in 
+        ///async functions for DART.
+        return this.auth.signInWithEmailAndPassword(email,password);
+    }
+
+    logout(){
+        return this.auth.signOut();
+    }
+
+}
+
+export default new Firebase();
--- src/login_screen/LoginScreen.js
+++ src/login_screen/LoginScreen.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './LoginScreen.css';
 import { Button } from "@material-ui/core";
 import { TextField } from "@material-ui/core";
-import { InputLabel } from '@material-ui/core';
 import { Paper } from "@material-ui/core";
+import Firebase from "../firebase";
 
 const LoginScreen = () => {
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const[emailIsValid, setEmailIsValid] = useState(null);
+    
+    const onLogin = async () => {
+        try{
+           await Firebase.login(email, password);
+        } catch (error) {
+            alert(error);
+        }
+    }
+
     return (
         <div className="background">
             <Paper elevation={13} className="card">
@@ -15,19 +27,24 @@ const LoginScreen = () => {
                             className="textfield"
                             id="emailTF"
                             label="Email"
-                            variant="outlined" />
+                            variant="outlined"
+                            onChange={(event) => setEmail(event.target.value)}
+                             />
                     </div>
                     <div className="bottom-margin">
                         <TextField
                             className="textfield"
+                            type="password"
                             id="passwordTF"
                             label="Password"
-                            variant="outlined" />
+                            variant="outlined"
+                            onChange={(event) => setPassword(event.target.value)} />
                     </div>
                     <div>
                         <Button
                             variant="contained"
-                            color="primary">
+                            color="primary"
+                            onClick={onLogin}>
                             Login
                         </Button>
                     </div>
