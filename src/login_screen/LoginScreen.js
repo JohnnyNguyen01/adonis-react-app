592f4897bf1c696471a2b2d49312e1a507901bc7
johnny <johnnynguyenwork@gmail.com>
2020-07-19 11:27:53 +1000

    completed LoginScreen UI

--- src/login_screen/LoginScreen.js
+++ src/login_screen/LoginScreen.js
@@ -1,32 +1,38 @@
 import React from 'react';
 import './LoginScreen.css';
-import { Card } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import { TextField } from "@material-ui/core";
 import { InputLabel } from '@material-ui/core';
-import { Grid } from "@material-ui/core";
-import {GridItem} from "@material-ui/core";
+import { Paper } from "@material-ui/core";
 
 const LoginScreen = () => {
     return (
         <div className="background">
-            <Card raised="true" className="card">
-                <Grid container spacing="true" alignItems="center" alignContent="center">
-
-                    <Grid item className="textField">
-                        <InputLabel>Username</InputLabel>
-                        <TextField id="username" type="text" inputProps={"a"} />
-                    </Grid >
-                    <div className="textField">
-                        <InputLabel>Password</InputLabel>
-                        <TextField id="password" type="text" inputProps={"a"} />
+            <Paper elevation={13} className="card">
+                <form className="form">
+                    <div className="bottom-margin">
+                        <TextField
+                            className="textfield"
+                            id="emailTF"
+                            label="Email"
+                            variant="outlined" />
+                    </div>
+                    <div className="bottom-margin">
+                        <TextField
+                            className="textfield"
+                            id="passwordTF"
+                            label="Password"
+                            variant="outlined" />
                     </div>
                     <div>
-
+                        <Button
+                            variant="contained"
+                            color="primary">
+                            Login
+                        </Button>
                     </div>
-
-                </Grid>
-
-            </Card>
+                </form>
+            </Paper>
         </div>);
 }
 
